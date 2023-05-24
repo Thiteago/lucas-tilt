80ff2e2e9498de5957baa4d7d3d111a6c37a41b2
Thiago <thiagodavidpd@gmail.com>
2023-05-24 17:54:33 -0300

    create layout to initialize firebase

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  import "../app.css";
+  import { initializeApp } from 'firebase/app';
+  import { getFirestore } from 'firebase/firestore';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBW3cdQoZIZkJWmZD56YGHLqOZ88YWoWiE",
+    authDomain: "lucasssss-688ef.firebaseapp.com",
+    projectId: "lucasssss-688ef",
+    storageBucket: "lucasssss-688ef.appspot.com",
+    messagingSenderId: "885300969563",
+    appId: "1:885300969563:web:cfaecc3101f5d48d2aff7e"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
+
+</script>
+
+<slot />
